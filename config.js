d266c0c5e67cb1eb138f93233bdc996a66f8a6f1
unknown <patelgaurav282000@gmail.com>
2024-07-25 16:51:15 +0530

    Email Authentication Using React Native Expo

---
+++ config.js
@@ -0,0 +1,22 @@
+// firebase config key setup
+
+import firebase from "firebase/compat/app";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// your web app's firebase configuration
+const firebaseConfig ={
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+}
+
+if(!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
+export {firebase};
